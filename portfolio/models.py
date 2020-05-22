db974ec99e3f4327cd3bd8d8320fdd4907e18d51
Renjie Zhu <renjie.j.zhu@gmail.com>
2020-05-21 23:21:13 -0700

    create experience model

--- portfolio/models.py
+++ portfolio/models.py
@@ -3,12 +3,13 @@ from django.db import models
 # Create your models here.
 
 class Experience(models.Model):
-    TYPE = [
+    EXP_TYPE = [
         ('project', 'project'),
         ('work', 'work'),
     ]
     title = models.CharField(max_length=200, null=True)
     description = models.TextField(null=True)
-    exp_type = models.CharField(null=True, choices=TYPE)
+    experience_type = models.CharField(null=True, choices=EXP_TYPE)
     start_time = models.DateField(null=True)
-    end_time = models.DateField(null=True)
+    end_time = models.DateField(null=True, blank=True)
+    github_url = models.URLField(null=True)
