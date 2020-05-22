f469dd29af5c18ed8d462fd40d7837a665496dea
Renjie Zhu <renjie.j.zhu@gmail.com>
2020-05-22 12:40:41 -0700

    add Post model and keyword model

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Keyword(models.Model):
+
+    kw = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.kw
+
 
 class Experience(models.Model):
     EXP_TYPE = [
@@ -10,9 +16,24 @@ class Experience(models.Model):
     title = models.CharField(max_length=200, null=True)
     description = models.TextField(null=True)
     experience_type = models.CharField(null=True, max_length=200, choices=EXP_TYPE)
+    keywords = models.ManyToManyField(Keyword)
     start_time = models.DateField(null=True)
     end_time = models.DateField(null=True, blank=True)
     github_url = models.URLField(null=True)
+    
 
     def __str__(self):
         return self.title
+
+
+class Post(models.Model):
+
+    title = models.CharField(max_length=200, null=True)
+    content = models.TextField(null=True)
+    keywords = models.ManyToManyField(Keyword)
+    date_published = models.DateTimeField(auto_now_add=True)
+    date_last_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
--- portfolio/views.py
+++ portfolio/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render
 
 from .models import *
 
-# Create your views here.
-
 
 def homepage(request):
     context = {
