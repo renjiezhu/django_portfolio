70bb48d61527a1a2514f10266dc2438eaa0c0402
Renjie Zhu <renjie.j.zhu@gmail.com>
2020-05-25 00:01:22 -0700

    use card to order projects

--- portfolio/models.py
+++ portfolio/models.py
@@ -12,14 +12,15 @@ class Experience(models.Model):
     EXP_TYPE = [
         ('project', 'project'),
         ('work', 'work'),
+        ('education', 'education')
     ]
     title = models.CharField(max_length=200, null=True)
     description = models.TextField(null=True)
     experience_type = models.CharField(null=True, max_length=200, choices=EXP_TYPE)
-    keywords = models.ManyToManyField(Keyword)
+    keywords = models.ManyToManyField(Keyword, blank=True)
     start_time = models.DateField(null=True)
     end_time = models.DateField(null=True, blank=True)
-    github_url = models.URLField(null=True)
+    github_url = models.URLField(null=True, blank=True)
     
 
     def __str__(self):
