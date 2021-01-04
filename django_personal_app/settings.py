ae8465efaf90f8bc4e9392a67c3b32fe6fe72b0b
Renjie Zhu <renjie.j.zhu@gmail.com>
2021-01-03 20:37:51 -0800

    remove secret key and other security related

--- django_personal_app/settings.py
+++ django_personal_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from .secrets import secret_key, allowed_hosts
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,17 +21,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'zp2a&7qi^!$upx!#ve_pis)71$wq7mpm&t@vf*53s+lqi#+5x$'
+SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 # DEBUG = False
 
-ALLOWED_HOSTS = [
-    # "*" # required when debug is False
-    # "www.renjiezhu.com"
-]
-
+ALLOWED_HOSTS = allowed_hosts
 
 # Application definition
 
